e41fbf5c54a94124937f4d29aadd39f7757a25a5
eliasvagan <elias.vagan@gmail.com>
2019-03-15 16:49:37 +0100

    TrenignsApp =  Main class

--- treningsdatabase/src/AppController.java
+++ treningsdatabase/src/AppController.java
@@ -1,10 +1,72 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+
 public class AppController {
-    @FXML public TextArea screen;
+    @FXML TextArea screen;
 
-    private String s = "Her er det tekst";
+    @FXML TextField aparatOvelse;
+    @FXML TextField aparatAparat;
+    @FXML TextField aparatKg;
+    @FXML TextField aparatSett;
 
-    @FXML
-    public void initialize(){
+    @FXML TextField utenOvelse;
+    @FXML TextArea utenBeskrivelse;
+
+    private String s;
+
+    public AppController() {
+        s = "Appen har startet";
+    }
+
+    @FXML public void initialize(){
+        screen.setText(s);
+    }
+
+    @FXML public void regOvingAparat(){
+        if (aparatOvelse.getText().equals("") ||
+            aparatAparat.getText().equals("") ||
+            aparatKg.getText().equals("") ||
+            aparatSett.getText().equals(""))
+        {
+            this.s = "Ett eller flere tomme felter.";
+            updateScreen();
+            return;
+        }
+        try {
+            Double.parseDouble(aparatKg.getText());
+            Integer.parseInt(aparatSett.getText());
+        } catch (Exception e) {
+            this.s = "Ulovlig input " + e.getMessage();
+            updateScreen();
+            return;
+        }
+        s = aparatOvelse.getText() + "\n" +
+            aparatAparat.getText() + "\n" +
+            aparatKg.getText() + "\n" +
+            aparatSett.getText();
+        updateScreen();
+    }
+
+    @FXML public void regOvingUten(){
+        if (utenOvelse.getText().equals("") ||
+            utenBeskrivelse.getText().equals(""))
+        {
+            s = "Ett eller flere tomme felter";
+            updateScreen();
+            return;
+        }
+        s = utenOvelse.getText() + "\n" +
+            utenBeskrivelse.getText();
+        updateScreen();
+
+    }
+
+    private void updateScreen() {
         screen.setText(s);
     }
 
--- treningsdatabase/src/TreningsApp.java
+++ treningsdatabase/src/TreningsApp.java
@@ -1,16 +1,30 @@
-package PACKAGE_NAME;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class TreningsApp extends Application {
+
+    private Stage primaryStage;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+        Thread.currentThread().setName("JavaFX");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("App.fxml"));
+
+        AnchorPane root = (AnchorPane) loader.load();
+        Scene scene = new Scene(root);
+
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
 
     }
 }
