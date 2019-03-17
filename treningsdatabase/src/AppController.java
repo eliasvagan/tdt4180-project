1bc989762706c09f031bff98d2b2ee5e2d65855f
eliasvagan <elias.vagan@gmail.com>
2019-03-17 21:17:36 +0100

    database attempt 1

--- treningsdatabase/src/AppController.java
+++ treningsdatabase/src/AppController.java
@@ -1,9 +1,9 @@
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AppController {
@@ -17,14 +17,23 @@ public class AppController {
     @FXML TextField utenOvelse;
     @FXML TextArea utenBeskrivelse;
 
-    private String s;
+    private DBConn dbconn;
+    private String errors = "";
+    private List<Ovelse> ovelser;
 
     public AppController() {
-        s = "Appen har startet";
+        this.ovelser = new ArrayList<Ovelse>();
+        // TODO: Få dette til å funke -> updateScreen("Appen har startet.");
     }
 
     @FXML public void initialize(){
-        screen.setText(s);
+        try {
+            this.dbconn.connect();
+        } catch(Exception e) {
+            this.errors = "Databasefeil: " + e.getMessage();
+            updateScreen();
+        }
+
     }
 
     @FXML public void regOvingAparat(){
@@ -33,7 +42,7 @@ public class AppController {
             aparatKg.getText().equals("") ||
             aparatSett.getText().equals(""))
         {
-            this.s = "Ett eller flere tomme felter.";
+            this.errors += "Ett eller flere tomme felter.";
             updateScreen();
             return;
         }
@@ -41,14 +50,17 @@ public class AppController {
             Double.parseDouble(aparatKg.getText());
             Integer.parseInt(aparatSett.getText());
         } catch (Exception e) {
-            this.s = "Ulovlig input " + e.getMessage();
+            this.errors += "Ulovlig input " + e.getMessage();
             updateScreen();
             return;
         }
-        s = aparatOvelse.getText() + "\n" +
-            aparatAparat.getText() + "\n" +
-            aparatKg.getText() + "\n" +
-            aparatSett.getText();
+        this.addOvelse(new Ovelse(
+                aparatOvelse.getText(),
+                aparatAparat.getText(),
+                Double.parseDouble(aparatKg.getText()),
+                Integer.parseInt(aparatSett.getText())
+        ));
+
         updateScreen();
     }
 
@@ -56,18 +68,35 @@ public class AppController {
         if (utenOvelse.getText().equals("") ||
             utenBeskrivelse.getText().equals(""))
         {
-            s = "Ett eller flere tomme felter";
+            errors = "Ett eller flere tomme felter";
             updateScreen();
             return;
         }
-        s = utenOvelse.getText() + "\n" +
-            utenBeskrivelse.getText();
+        this.addOvelse(new Ovelse(
+                utenOvelse.getText(),
+                utenBeskrivelse.getText()
+        ));
         updateScreen();
 
     }
 
-    private void updateScreen() {
+    private void addOvelse(Ovelse o) {
+        this.ovelser.add(o);
+    }
+    private void updateScreen(String message) {
+        String s = message;
+        s += ("\n" + this.errors);
         screen.setText(s);
+        this.errors = "";
+    }
+    private void updateScreen() {
+        StringBuilder s = new StringBuilder();
+        this.ovelser.forEach((e) -> s.append(e.toString() + "\n"));
+
+        s.append("\n" + this.errors);
+
+        screen.setText(s.toString());
+        this.errors = "";
     }
 
 
--- treningsdatabase/src/Ovelse.java
+++ treningsdatabase/src/Ovelse.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
-
 public class Ovelse {
+
+    private final char type;
+
+    private String name;
+    private String aparat;
+    private double kg;
+    private int sett;
+
+    private String beskrivelse;
+
+    public Ovelse(String name, String aparat, double kg, int sett) {
+        this.type = 'A'; // Aparat
+        this.name = name;
+        this.aparat = aparat;
+        this.kg = kg;
+        this.sett = sett;
+    }
+
+    public String toString() {
+        if (this.type == 'A') {
+            return this.name + ", " + this.aparat + ", " + this.kg + "kg * " + this.sett;
+        } else {
+            return this.name + ", " + this.beskrivelse;
+        }
+    }
+
+    public Ovelse(String name, String beskrivelse) {
+        this.type = 'U'; //Uten aparat
+        this.name = name;
+        this.beskrivelse = beskrivelse;
+    }
+
+    public String getType() {
+        return Character.toString(type);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAparat() {
+        return aparat;
+    }
+
+    public double getKg() {
+        return kg;
+    }
+
+    public int getSett() {
+        return sett;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
 }
