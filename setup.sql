a0e6630ff68db2de6c52d6f623e0db83519e5e71
laerva1801 <laerva1801@gmail.com>
2019-03-19 18:42:28 +0100

    Endret på modelleringen av øvelser, har nå ett table for alle øvinger uavhengig av type. Implementert en bool for å indikere om øvelsen krever apparat eller ikke. Fikset primærnøkler der det manglet.

--- setup.sql
+++ setup.sql
@@ -2,81 +2,65 @@ create database treningsdagbok; /* Er ikke sikkert du trenger disse to linjene *
 use treningsdagbok; /* Er ikke sikkert du trenger disse to linjene */
 
 create table treningsokt(
-	oktid int not null,
-  primary key (oktid),
-  dato date,
-  tidspunkt time,
-  varighet time,
-  form int,
-  prestasjon int
+                          oktid int not null AUTO_INCREMENT primary key,
+                          dato date,
+                          tidspunkt time,
+                          varighet time,
+                          form int,
+                          prestasjon int
   /* TODO: Feil her -> check (form >= 1 and form <= 11)*/
 );
 
 create table oktnotat(
-	oktid int not null,
-  notatid int not null,
-  notatinformasjon text,
-  foreign key (oktid) references treningsokt(oktid),
-  primary key(oktid, notatid)
+                       oktid int not null,
+                       notatid int not null  AUTO_INCREMENT,
+                       notatinformasjon text,
+                       foreign key (oktid) references treningsokt(oktid),
+                       primary key(oktid, notatid)
 );
 
 create table ovelse(
-	ovelseID int not null,
-	navn text,
-  primary key(ovelseID)
+                     ovelseID int not null AUTO_INCREMENT primary key,
+                     navn text,
+                     antallkg int,
+                     aparat boolean,
+                     antallSett int,
+                     apparatID int,
+                     tekstBeskrivelse text,
+                     foreign key (apparatid) references apparat(apparatid)
 );
 
 create table apparat(
-	apparatid int not null,
-  apparatnavn text,
-  apparatbrukbeskrivelse text,
-  primary key (apparatid)
-);
-
-create table ovelseMedApparat(
-	navn text,
-    ovelseID int not null,
-    antallkg int,
-    antallSett int,
-    apparatID int not null,
-    foreign key (apparatid) references apparat(apparatid),
-    foreign key (ovelseid) references ovelse(ovelseid),
-    primary key (ovelseid)
-);
-
-create table ovelseUtenApparat(
-	navn text,
-  ovelseid int not null,
-  tekstBeskrivelse text,
-  foreign key (ovelseid) references ovelse(ovelseid),
-  primary key (ovelseid)
+                      apparatid int not null  AUTO_INCREMENT primary key,
+                      apparatnavn text,
+                      apparatbrukbeskrivelse text
 );
 
 create table treningsoktOvelse(
-	oktid int not null,
-  ovelseid int not null,
-  foreign key (oktid) references treningsokt(oktid),
-  foreign key (ovelseid) references ovelse(ovelseid),
-  primary key (oktid, ovelseid)
+                                oktid int not null,
+                                ovelseid int not null,
+                                foreign key (oktid) references treningsokt(oktid),
+                                foreign key (ovelseid) references ovelse(ovelseid),
+                                primary key (oktid, ovelseid)
 );
 
 create table ovelsesGruppe(
-	ovelsesgruppeid int not null,
-  ovelsesgruppenavn text,
-  primary key (ovelsesgruppeid)
+                            ovelsesgruppeid int not null AUTO_INCREMENT,
+                            ovelsesgruppenavn text,
+                            primary key (ovelsesgruppeid)
 );
 
 create table element(
-	ovelsesgruppeid int not null,
-  element VARCHAR(200) not null,
-  foreign key (ovelsesgruppeid) references ovelsesgruppe(ovelsesgruppeid),
-  primary key (ovelsesgruppeid, element)
+                      ovelsesgruppeid int not null,
+                      element VARCHAR(200) not null,
+                      foreign key (ovelsesgruppeid) references ovelsesgruppe(ovelsesgruppeid),
+                      primary key (ovelsesgruppeid, element)
 );
 
 create table ovelsesGruppeOvelseRelasjon(
-	ovelseid int not null,
-  ovelsesgruppeid int not null,
-  foreign key (ovelseid) references ovelse(ovelseid),
-  foreign key (ovelsesgruppeid) references ovelsesgruppe(ovelsesgruppeid),
-	primary key (ovelseid, ovelsesgruppeid)
+                                          ovelseid int not null,
+                                          ovelsesgruppeid int not null,
+                                          foreign key (ovelseid) references ovelse(ovelseid),
+                                          foreign key (ovelsesgruppeid) references ovelsesgruppe(ovelsesgruppeid),
+                                          primary key (ovelseid, ovelsesgruppeid)
 );
